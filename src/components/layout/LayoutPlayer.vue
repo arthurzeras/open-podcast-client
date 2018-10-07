98869e09faa3b450658a2206cf764971f69d77d2
arthurzeras <arthurolvmorais@gmail.com>
2018-10-07 10:30:45 -0300

    Adicionando progress-bar e indicacao de tempo

--- src/components/layout/LayoutPlayer.vue
+++ src/components/layout/LayoutPlayer.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="player">
+    <div class="player-bar"></div>
+    <div class="player-bar-indicator"></div>
+    <div class="player-bar-point"></div>
     <div class="row text-center no-gutters player-container">
       <div class="col podcast-info">
         <div class="row no-gutters">
@@ -16,12 +19,17 @@
           </div>
         </div>
       </div>
-      <div class="col-6 player-controls">
-        <div class="material-icons">fast_rewind</div>
-        <div class="material-icons">play_arrow</div>
-        <div class="material-icons">fast_forward</div>
+      <div class="col-6 player-main flex-center-column">
+        <div class="player-time flex-center">
+          00:00:00 / 01:15:23
+        </div>
+        <div class="player-controls flex-center">
+          <div class="material-icons">replay_10</div>
+          <div class="material-icons">play_arrow</div>
+          <div class="material-icons">forward_10</div>
+        </div>
       </div>
-      <div class="col player-details">
+      <div class="col player-details flex-center">
         <div class="row no-gutters w-100">
           <div class="col-8">
             <div class="player-volume row no-gutters">
@@ -54,13 +62,55 @@ export default {
 <style lang="scss" scoped>
 @import '../../assets/scss/variables';
 
+@mixin bar($color) {
+  width: 100%;
+  height: 4px;
+  display: block;
+  cursor: pointer;
+  position: relative;
+  background-color: $color;
+}
+
+@mixin bar-indicator() {
+  top: 0;
+  left: 0;
+  height: 4px;
+  position: absolute;
+  background-color: $main;
+}
+
+@mixin bar-point() {
+  left: 0;
+  width: 10px;
+  z-index: 100;
+  height: 10px;
+  display: block;
+  transition: .4s;
+  cursor: pointer;
+  position: absolute;
+  border-radius: 100%;
+  background-color: $main;
+  border: 1px solid $white;
+}
+
 #player {
   left: 0;
   bottom: 0;
   width: 100%;
-  height: 70px;
+  height: 80px;
   position: absolute;
   background-color: $dark;
+
+  .player-bar {
+    @include bar($main-dark);
+  }
+  .player-bar-indicator {
+    @include bar-indicator();
+  }
+  .player-bar-point {
+    @include bar-point();
+    top: -3px;
+  }
 
   .material-icons {
     transition: .4s;
@@ -91,53 +141,36 @@ export default {
         }
       }
     }
-    .player-controls {
-      height: 70px;
-      @include flex-center();
-    }
-  }
-  .player-controls {
-    .material-icons {
-      font-size: 25pt;
-      padding: 0 10px;
-    }
-  }
-  .player-details {
-    @include flex-center();
-    .player-volume {
-      .material-icons {
-        cursor: pointer;
-        &:hover {
-          color: $main;
+    .player-main {
+      .player-time {
+        font-weight: bold;
+        margin-top: 5px;
+      }
+      .player-controls {
+        height: 70px;
+        .material-icons {
+          font-size: 25pt;
+          padding: 0 10px;
         }
       }
-      .volume-bar {
-        width: 100%;
-        height: 4px;
-        display: block;
-        cursor: pointer;
-        position: relative;
-        background-color: $darker;
-      }
-      .volume-bar-indicator {
-        top: 0;
-        left: 0;
-        height: 4px;
-        position: absolute;
-        background-color: $main;
-      }
-      .volume-bar-point {
-        left: 0;
-        width: 10px;
-        z-index: 100;
-        height: 10px;
-        display: block;
-        transition: .4s;
-        cursor: pointer;
-        position: absolute;
-        border-radius: 100%;
-        background-color: $main;
-        border: 1px solid $white;
+    }
+    .player-details {
+      .player-volume {
+        .material-icons {
+          cursor: pointer;
+          &:hover {
+            color: $main;
+          }
+        }
+        .volume-bar {
+          @include bar($darker)
+        }
+        .volume-bar-indicator {
+          @include bar-indicator();
+        }
+        .volume-bar-point {
+          @include bar-point();
+        }
       }
     }
   }
